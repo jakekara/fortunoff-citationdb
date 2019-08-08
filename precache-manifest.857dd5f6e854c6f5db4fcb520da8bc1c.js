8ff7f5e1234475191f7e8a2ae9b4adf0ad9319fb
Jake Kara <jake@jakekara.com>
2019-08-08 16:24:39 -0400

    Updates

---
+++ precache-manifest.857dd5f6e854c6f5db4fcb520da8bc1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7aed7e4723faf4dd72c50b0e411f53",
+    "url": "/fortunoff-citationdb/index.html"
+  },
+  {
+    "revision": "60a2c8cb7bc52323af40",
+    "url": "/fortunoff-citationdb/static/css/main.2b1458a8.chunk.css"
+  },
+  {
+    "revision": "bf295abcc3826ba23981",
+    "url": "/fortunoff-citationdb/static/js/2.18f209f3.chunk.js"
+  },
+  {
+    "revision": "60a2c8cb7bc52323af40",
+    "url": "/fortunoff-citationdb/static/js/main.6ccd6991.chunk.js"
+  },
+  {
+    "revision": "0c9a0596cdb6ed6320e0",
+    "url": "/fortunoff-citationdb/static/js/runtime~main.54ce4746.js"
+  }
+]);
